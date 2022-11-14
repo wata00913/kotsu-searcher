07b590e10b7a2296769c6630879f9fee18e52eea
wata00913 <175d8639@gmail.com>
2022-11-15 07:19:36 +0900

    時刻表の検索処理を途中まで実装

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const ekispert = require("./lib/ekispert");
+
+main();
+
+function main() {
+  if (process.env.EKISPERT_ACCESS_KEY === undefined) {
+    console.log("環境変数:EKISPERT_ACCESS_KEYを設定してください。");
+    return;
+  }
+
+  const timetable = require('./lib/serach_command/timetable');
+  timetable.module();
+}
