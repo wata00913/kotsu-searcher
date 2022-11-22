abe63eafbe0a4ad594d8d14c7d451dd6f2880d6c
wata00913 <175d8639@gmail.com>
2022-11-23 08:18:43 +0900

    オプションのパース処理を追加

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 #!/usr/bin/env node
+const arg = require("arg");
 const ekispert = require("./lib/ekispert");
+const { Duration } = require("luxon");
+
+const optionSettings = {
+  "--hour": Number,
+  "--from": String,
+  "--to": String,
+  "--daydiagram": String,
+};
+
+const commandDefaultOptions = {
+  timetable: {
+    "daydiagram": "weekday",
+  },
+};
+
+const successResult = { success: true, message: "" };
+
+const validation = {
+  timetable: {
+    "daydiagram": (val) => {
+      if (!["weekday", "saturday", "holiday"].includes(val)) {
+        return { success: false, message: "diagramの値が不正です" };
+      }
+      return successResult;
+    },
+
+    "hour": (val) => {
+      if (isNaN(val) || val < 0)
+        return { success: false, message: "時刻の値が不正です" };
+      return successResult;
+    },
+
+    "from": function (val) {
+      return this.timeValid(val);
+    },
+    "to": function (val) {
+      return this.timeValid(val);
+    },
+
+    timeValid(timeStr) {
+      const time = Duration.fromISOTime(timeStr);
+      if (!Object.keys(time.toObject()).length)
+        return { success: false, message: "時刻が不正です" };
+      return successResult;
+    },
+  },
+};
 
 main();
 
@@ -9,6 +57,41 @@ function main() {
     return;
   }
 
-  const timetable = require('./lib/serach_command/timetable');
-  timetable();
+  const timetable = require("./lib/serach_command/timetable");
+  try {
+    const [command, options] = parseOptions();
+    timetable(options);
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+}
+
+function parseOptions() {
+  const args = arg(optionSettings);
+  const command = args["_"][0];
+  delete args["_"];
+
+  const normalize = (obj, propName) => {
+    const reg = /^--(\w+)/;
+    const match = propName.match(reg);
+    if (!match) return;
+
+    obj[match[1]] = obj[match[0]];
+    delete obj[match[0]];
+  };
+  Object.keys(args).forEach((key) => normalize(args, key));
+
+  if (!commandDefaultOptions.hasOwnProperty(command))
+    throw "コマンドを指定してください";
+
+  const options = { ...commandDefaultOptions[command], ...args };
+  const validators = validation[command];
+
+  for (key in options) {
+    const result = validators[key](options[key]);
+    if (!result.success) throw result.message;
+  }
+
+  return [command, options];
 }
